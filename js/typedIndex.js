9a7478f256bb56da00dd4ac8dbdb0a4445192492
Lautaro Zapata <oratualzc@gmail.com>
2023-01-23 01:31:48 -0300

    Initial commit

---
+++ js/typedIndex.js
@@ -0,0 +1,14 @@
+const typed = new Typed('.typed',{
+    strings: ['ESPACIALES','ESPECIALES'],
+    typeSpeed: 80,
+    startDelay: 300,
+    backSpeed: 80,
+    smartBackspace: false,
+    shuffle: false,
+    backDelay: 300,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+});
